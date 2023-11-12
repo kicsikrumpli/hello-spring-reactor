d02d45a91f46d96fe1b4c7936da1ba46d33cf1da
daniel david kovacs <kicsikrumpli@gmail.com>
2023-11-12 15:37:22 +0100

    removed comment

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -53,32 +53,6 @@ public class DemoApplication implements CommandLineRunner {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    /**
-     * Reactive Example.
-     *
-     * - uses reactive repo: read - write json files for each document
-     * - reactive clients:
-     *      - thing client: make request with one thing
-     *      - attach client: make one request per doc
-     * - source of things, docs: existing events in repo
-     * - processing flow:
-     *      - make thing request
-     *          - from thing event payload
-     *          - if fails: skip attach requests
-     *          - if succeeds: continue
-     *      - make attach request
-     *          - from successful thing response and
-     *          - from payload of attach event: docs
-     *          - many requests, one per doc
-     *          - failing attach requests are skipped, ignore error
-     *      - combine thing response, attach response
-     *          - in case of failure write fail event into repo
-     *          - in case of success write success event into repo
-     * Usage:
-     * - To make a thing request fail: use "!" as prefix (see writeExample())
-     * - To make a doc attach fail: suffix doc name with "!" (see writeExample())
-     * - define groupId
-     */
     @Override
     public void run(String... args) throws Exception {
         String groupId = "12345B";
